40d511ae25d68b33c7fa4b5e2a58b24b7dbe6716
Ian Concepcion <ian03concepcion@gmail.com>
2018-05-19 22:28:00 +0800

    asasdas

---
+++ js/about.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	// $('#slider_section').fullpage({
+		// sectionsColor: ['#475577', '#939FAA', '#4A6FB1'],
+		// scrollOverflow: false,
+		// scrollOverflowReset: false,
+	// 	autoScrolling: false,
+	// 	fitToSection:false,
+	// 	responsiveWidth: 800,
+	// 	onLeave: function(index, nextIndex, direction){
+ //            if (nextIndex != 1){
+ //                $('.main-header').addClass('sitcky-header');
+ //            } else {
+ //                $('.main-header').removeClass('sitcky-header');
+ //            }
+ //        }
+	// });
+
+
+ //    $('.main_header_innperpage').on("scroll", function() {
+	//     if($('.main_header_innperpage').scrollTop() > 1) {
+	//     	$('.main-header').addClass('sitcky-header');
+	//     }
+	//     else {
+	//     	$('.main-header').removeClass('sitcky-header');
+	//     }
+	// });
+});
